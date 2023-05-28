f0429fd9d108f86d0a23314a654002542bb062e3
Efkan Baser <118986513+MedaLenor@users.noreply.github.com>
2023-05-28 23:33:32 +0300

    Functions for one time and displays it on console.

--- app.js
+++ app.js
@@ -3,35 +3,36 @@ let operator = "";
 let n1 = 0;
 let n2 = 0;
 let matchResult = inputString.match(/\d+/)
+let result = 0;
 
 function add(n1, n2){
-    return n1 += n2;
+    result = n1+n2;
+    return result;
 }
 function subtract(n1, n2){
-    return n1 -= n2;
+    result = n1-n2;
+    return result;
 }
 function multiply(n1, n2){
-    return n1 *= n2;
+    result = n1*n2;
+    return result;
 }
 function divide(n1, n2){
-    return n1 /= n2;
+    result = n1/n2;
+    return result;
 }
 
-function addValues (number){
-    
-}
-
-function operate(n1, operator, n2){
-    if(operator === "+"){
+function operate(n1, n2, operator){
+    if(operator == "+"){
         add(n1, n2)
     }
-    else if(operator === "-"){
+    else if(operator == "-"){
         subtract(n1, n2)
     }
-    else if(operator === "*"){
+    else if(operator == "*"){
         multiply(n1, n2)
     }
-    else if(operator === "-"){
+    else if(operator == "/"){
         divide(n1, n2)
     }
 }
@@ -65,26 +66,29 @@ function createButtons(amount) {
         'border: 1px solid black; border-radius: 10px; width: calc((100% / 4) - 10px); height: 50px; margin: 5px 5px 10px 5px;cursor: pointer;';
     button.addEventListener('click', () => {
         inputString += button.textContent;
-        if(button.textContent == "+" ||button.textContent == "/" ||button.textContent == "*" ||button.textContent == "-") {
+        if(button.textContent == "+" ||button.textContent == "/" ||button.textContent == "*" ||button.textContent == "-"){
+
         if(n1 == 0){
-        n1 = parseInt(inputString.slice(0, length-1));
+        n1 = parseInt(inputString.slice(0, inputString.length-1));
         }
+
         else if(n1 != 0){
-        n2 = parseInt(inputString.slice(0, length-1));
+        n2 = parseInt(inputString.slice(0, inputString.length-1));
         }
+
         operator = inputString.replace(/\d+/, '');
         inputString = ""
-        if(n1 != 0 && n2 != 0){
-            operate()
         }
+
+        else if(button.textContent == "="){
+        n2 = parseInt(inputString.slice(0, inputString.length-1));
+        inputString = ""
+        operate(n1, n2, operator);
+        console.log(result);
         }
-        console.log(n1)
-        console.log(n2)
-        console.log(operator)
     });
       buttonsDisplay.appendChild(button);
     }
   }
   
   createButtons(16);
-  
